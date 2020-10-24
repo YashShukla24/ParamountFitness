b7857ee833ca53395514d430fa2d059be0043760
yashshukla24 <yashofficial24nov@gmail.com>
2020-10-24 17:07:15 +0530

    Updates

---
+++ precache-manifest.d2c35bb95ba3c51cfab01a90eee4244c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "d800c29897743050bda6c01a69f4debc",
+    "url": "/ParamountFitness/static/media/banner-sample.d800c298.png"
+  },
+  {
+    "revision": "e98afa402365f5b40b94",
+    "url": "/ParamountFitness/static/css/main.33cfd208.chunk.css"
+  },
+  {
+    "revision": "8630d4301e7b2ccf7616",
+    "url": "/ParamountFitness/static/js/runtime~main.8630d430.js"
+  },
+  {
+    "revision": "98de8ac8bf1ce7b322cb9fdb5875d980",
+    "url": "/ParamountFitness/static/media/headerdemo.98de8ac8.jpg"
+  },
+  {
+    "revision": "6c9d9a4f7d1db1bb3f23",
+    "url": "/ParamountFitness/static/js/2.6c9d9a4f.chunk.js"
+  },
+  {
+    "revision": "e98afa402365f5b40b94",
+    "url": "/ParamountFitness/static/js/main.e98afa40.chunk.js"
+  },
+  {
+    "revision": "17f8230806b01e719489c96b54f43fb1",
+    "url": "/ParamountFitness/static/media/benner.17f82308.jpg"
+  },
+  {
+    "revision": "8b0ea0fa36a1d5856fbde8674eb727f2",
+    "url": "/ParamountFitness/static/media/zymhomeproimg.8b0ea0fa.jpg"
+  },
+  {
+    "revision": "3b0d762258cfcbe35e404fecf74f1b97",
+    "url": "/ParamountFitness/static/media/aboutprozym.3b0d7622.jpg"
+  },
+  {
+    "revision": "6c9d9a4f7d1db1bb3f23",
+    "url": "/ParamountFitness/static/css/2.a6f7ab4f.chunk.css"
+  },
+  {
+    "revision": "8b9d3d06a893e2f3732338a47b4191c1",
+    "url": "/ParamountFitness/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/ParamountFitness/precache-manifest.d2c35bb95ba3c51cfab01a90eee4244c.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/ParamountFitness/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
